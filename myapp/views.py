22b8540d80cd0e4e94e74152288f6953c0630b03
hayeounglee <95111439+hayeounglee@users.noreply.github.com>
2024-08-25 15:48:37 +0900

    feat: index와 HTMLTemplate 분리

--- myapp/views.py
+++ myapp/views.py
@@ -6,25 +6,38 @@ topics = [
   {'id':3, 'title': 'model', 'body': 'Model is ..'}
 ]
 
-
-def index(request):
+def HTMLTemplate(articleTage):
   global topics
   ol = ''
   for topic in topics:
     ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-  return HttpResponse(f'''
+  return f'''
   <html>
     <body>
         <h1> Django </h1>
         <ol>
           {ol}           
         </ol>
+        {articleTage}
+        <a href="create/">create</a>
     </body>
   </html>
-  ''')
+  '''
+
+def index(request):
+  article='''
+  <h2>Welcome</h2>
+  Hello, Django
+'''
+  return(HttpResponse(HTMLTemplate(article)))
 
 def create(request):
-  return HttpResponse("Create")
+  article = '''
+    <p><input type="text" name="title" placeholder="title"></input></p>
+    <p><textarea name="body" placeholder="body"></textarea></p>
+    <p><input type="submit"></input></p>
+'''
+  return HttpResponse(HTMLTemplate(article))
 
 def read(request, id):
   return HttpResponse('Read!'+ id)
